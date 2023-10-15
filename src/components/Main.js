4e798c834ca3d90f4cfa0fe50fdbd9e1b923b022
meetbeke <meetbeke@gmail.com>
2023-10-15 16:05:09 +0300

    some chages

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,17 @@ function Main(){
             <section className='main-sectionA'>
                 <h3 className='main-title'>Little Lemon</h3>
                 <h4 className='main-sub-title'>Chicago</h4>
-                <p className='main-text'>We are family owned Medetranian restaurant, focused on traditional receipes
+                <p className='main-text'>We are family owned Medetranian restaurant, focused on
+                 traditional receipes
                     served with a modern twist.
+                    The menu at Little Lemon features a variety of dishes, including small plates, 
+                    entrees, and desserts. Some of the popular dishes include the hummus, 
+                    the grilled octopus, and the lemon chicken. 
+                    The restaurant also has a good selection of wines and cocktails. 
+                    Little Lemon is a great place to go for a casual meal or a special occasion. 
+                    The restaurant is also a good choice for people with dietary restrictions, 
+                    as it has a number of vegetarian and gluten-free options.
+
                 </p>
                 <button className='btn-reserve'>Reserve a Table</button>
                 <img src={restaurantfood} height="200px" width="200px"   alt="restaurant food"/>
